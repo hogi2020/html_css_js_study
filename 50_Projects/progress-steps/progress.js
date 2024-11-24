1b4892e156eff7782aabbc39d4edfb1b7074cfb3
ojm <junmyung2020@gmail.com>
2024-11-24 23:44:47 +0900

    Update side practice
    Update side practice

--- 50_Projects/progress-steps/progress.js
+++ 50_Projects/progress-steps/progress.js
@@ -1,29 +1,2 @@
-const progress = document.getElementById('progress')
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const circles = document.querySelectorAll('.circle')
 
-let currentActive = 1
-
-
-next.addEventListener('click', () => {
-    currentActive++
-
-    if(currnetActive > circles.length) {
-        currentActive = circles.length
-    }
-
-    update()
-})
-
-
-next.addEventListener('click', () => {
-    currentActive--
-
-    if(currnetActive < 1) {
-        currentActive = 1
-    }
-
-    update()
-})
 
